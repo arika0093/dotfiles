c50b3bd60f0f84762a55d0490cd64107d00d29d4
Arika Ishinami <delete0093@gmail.com>
2024-02-29 15:18:15 +0000

    install package change (add curl, without spin)

--- install-packages/library.sh
+++ install-packages/library.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+install_package=(
+  # programmer utility
+  vim
+  git
+  curl
+  # for use jetbrains mono install
+  fontconfig
+  unzip
+)
+
 # install apt package 
 sudo apt update &&
-sudo apt install -y \
-  vim \
-  git \
-  fontconfig unzip # fot use jetbrains mono install
+sudo apt install -y "${install_package[@]}"
 
 # install spin 
 # https://serversideup.net/open-source/spin/installation/install-linux
-if [ ! -e "$HOME/.spin" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/serversideup/spin/main/tools/install.sh)" || true
-else
-  echo 'spin is already installed.'
-fi
+# if [ ! -e "$HOME/.spin" ]; then
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/serversideup/spin/main/tools/install.sh)" || true
+# else
+#   echo 'spin is already installed.'
+# fi
 
 # if folder `HOME/.spin` is not exists, then call shell 
 
